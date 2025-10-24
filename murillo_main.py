7501dd4c77450da4ce0132e0d0069fdeefa15813
jmuri9753 <muri9753@student.acitech.org>
2025-10-24 10:46:53 -0400

    Finished validation when user picks numbers for their list

--- murillo_main.py
+++ murillo_main.py
@@ -18,27 +18,27 @@ def main():
               "[-] 1. Number Range Selection \n"
               "[-] 2. Sorting Methods \n")
 
-        selection = int(input("What Do You Want To Do? Please Input A Number :) : "))
+        selection = int(input("What Do You Want To Do? Please Input The Number For Your Choice :) : "))
 
         os.system('cls' if os.name == 'nt' else 'clear')
 
         def validate_input():
             while True:
                 try:
+
                     minimum_value = int(input("[-] What would you like as your minimum number/value? "))
                     maximum_value = int(input("[-] What would you like as your maximum number/value? "))
+                    list_range = int(input("[-] How many numbers do you want in your list? "))
 
                     if minimum_value >= maximum_value:
                         print("[!] MIN & MAX ERROR! MIN VALUE HAS TO BE LESS THAN MAX VALUE! PLEASE TRY AGAIN")
-                    if maximum_value > minimum_value:
-                        return minimum_value, maximum_value
 
-                    list_range = int(input("[-] How many numbers do you want in your list? "))
+                    if maximum_value > minimum_value and list_range > 0:
+                        return minimum_value, maximum_value, list_range
 
-                    if list_range > 0:
-                        return list_range
-                    else:
+                    if list_range < 0:
                         print("[!] RANGE ERROR! PLEASE INPUT A POSITIVE INTEGER MORE THAN 0! PLEASE TRY AGAIN")
+
                 except:
                     print("[!] ERROR! PLEASE INPUT A INTEGER")
 
@@ -49,12 +49,8 @@ def main():
             os.system('cls' if os.name == 'nt' else 'clear')
             print("[-] In here, you choose the range of your numbers and how many numbers you want in that range for "
                   "your list!")
-            validate_input()
 
-
-            user_min = validate_input()
-            user_max = validate_input()
-            user_range = validate_input()
+            user_min, user_max, user_range = validate_input()
             print("[-] Your numbers in your list will be from ", str(user_min), "-", str(user_max), "and it will have",
                   str(user_range), "numbers!")
 
@@ -65,17 +61,29 @@ def main():
                   "[-] 2. Insertion Sort Method \n"
                   "[-] 3. Selection Sort Method \n")
             sub_selection = int(input("Please input the number for the sorting you would like to use for your list : "))
+
             if sub_selection == 0:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 selection = 0
             if sub_selection == 1:
                 os.system('cls' if os.name == 'nt' else 'clear')
                 values = [random.randint(user_min, user_max) for i in range(user_range)]
-                print("Before Sorting : ", values)
+                print("List before Sorting : ", str(values))
                 bubble_sort(values)
-                print("After Soring : ", values)
-                something = int(input(""))
+                print("List after Sorting : ", str(values))
+
+            if sub_selection == 2:
                 os.system('cls' if os.name == 'nt' else 'clear')
+                values = [random.randint(user_min, user_max) for i in range(user_range)]
+                print("List before Sorting : ", str(values))
+                insertion_sort(values)
+                print("List after Sorting : ", str(values))
+            if sub_selection == 3:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                values = [random.randint(user_min, user_max) for i in range(user_range)]
+                print("List before Sorting : ", str(values))
+                selection_sort(values)
+                print("List after Sorting : ", str(values))
 
 
 if __name__ == "__main__":
